33471c446ce8d71bb02f679c7f5363fb1fda11b5
Didi <didiswalakutty@gmail.com>
2023-08-16 18:36:58 +0200

    problem with movement

--- src/load_player_sprites.c
+++ src/load_player_sprites.c
@@ -6,7 +6,7 @@
 /*   By: diwalaku <diwalaku@student.42.fr>            +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/08/11 19:09:09 by diwalaku      #+#    #+#                 */
-/*   Updated: 2023/08/11 22:21:29 by diwalaku      ########   odam.nl         */
+/*   Updated: 2023/08/16 12:44:00 by diwalaku      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ t_game	*load_player_up(mlx_t *mlx, t_game *img)
 {
 	img->player_up_t = mlx_load_png("sprites/player_up.png");
 	if (!img->player_up_t)
-		exit_error("Couldn't load player png");
+		exit_error("Couldn't load player_up png");
 	img->player_up = mlx_texture_to_image(mlx, img->player_up_t);
 	if (!img->player_up)
 		exit_error("Couldn't create player_up image");
@@ -27,7 +27,7 @@ t_game	*load_player_down(mlx_t *mlx, t_game *img)
 {
 	img->player_down_t = mlx_load_png("sprites/player_down.png");
 	if (!img->player_down_t)
-		exit_error("Couldn't load player png");
+		exit_error("Couldn't load player_down png");
 	img->player_down = mlx_texture_to_image(mlx, img->player_down_t);
 	if (!img->player_down)
 		exit_error("Couldn't create player_down image");
@@ -38,7 +38,7 @@ t_game	*load_player_left(mlx_t *mlx, t_game *img)
 {
 	img->player_left_t = mlx_load_png("sprites/player_left.png");
 	if (!img->player_left_t)
-		exit_error("Couldn't load player png");
+		exit_error("Couldn't load player_left png");
 	img->player_left = mlx_texture_to_image(mlx, img->player_left_t);
 	if (!img->player_left)
 		exit_error("Couldn't create player_left image");
@@ -49,9 +49,20 @@ t_game	*load_player_right(mlx_t *mlx, t_game *img)
 {
 	img->player_right_t = mlx_load_png("sprites/player_right.png");
 	if (!img->player_right_t)
-		exit_error("Couldn't load player png");
+		exit_error("Couldn't load player_right png");
 	img->player_right = mlx_texture_to_image(mlx, img->player_right_t);
 	if (!img->player_right)
 		exit_error("Couldn't create player_right image");
+	return (img);
+}
+
+t_game	*load_gameover(mlx_t *mlx, t_game *img)
+{
+	img->gameover_t = mlx_load_png("sprites/gameover.png");
+	if (!img->gameover_t)
+		exit_error("Couldn't load gameover png");
+	img->gameover = mlx_texture_to_image(mlx, img->gameover_t);
+	if (!img->gameover)
+		exit_error("Couldn't create gameover image");
 	return (img);
 }
---
+++ src/window.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   window.c                                           :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: diwalaku <diwalaku@student.42.fr>            +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2023/08/16 11:09:08 by diwalaku      #+#    #+#                 */
+/*   Updated: 2023/08/16 17:21:24 by diwalaku      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../so_long.h"
+// print moves and collectables in window?
+// update game
+
+void	end_game(t_game *game)
+{
+	int	x;
+	int	y;
+	mlx_instance_t *ending;
+
+	x = x * PIXELS;
+	y = y * PIXELS;
+	ending = &game->player->instances[0];
+	ending->enabled = false;
+	game->death = 1;
+	mlx_image_to_window(game->mlx, game->gameover, (game->width_x / 2) * PIXELS, (game->height_y / 2) * PIXELS);
+	ft_printf("Game Over, congratulations!\n");
+}
+
+void	update_game(t_game *game)
+{
+	ft_printf("%i moves\n", game->steps);
+	if (game->player_x == 'C' && game->player_y == 'C')
+		{
+			remove_collectable(game, game->player_x, game->player_y);
+			game->data[game->player_y][game->player_x] = '0';
+			game->total_collect += 1;
+		}
+	if (game->total_collect == game->count_c \
+	&& game->player_x == 'E' && game->player_y == 'E')
+	{
+		end_game(game);
+		mlx_close_window(game->mlx);
+	}
+}
